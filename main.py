11d50af9fc61d6b68ed159ddd57c325c407deb27
Roger <eitnerjw@gmail.com>
2023-01-16 01:00:35 -0500

    improvements

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class ModManager:
 			
 	def get_latest_mod(self):
 		try:
-			response = requests.get(self.RELEASE_URI)
+			response = requests.get(RELEASE_URI)
 			response.raise_for_status()
 		except requests.exceptions.RequestException as err:
 			print("Error Occured: ",err)
@@ -47,7 +47,7 @@ class ModManager:
 		Compares local mod version to remote mod version
 		"""
 		if self.local_mod_exists():
-			tree = ET.parse(self.localManifestPath)
+			tree = ET.parse(self.localManifestPath())
 			root = tree.getroot()
 			published_utc = int(root.get('published_utc'))
 		else:
@@ -56,67 +56,68 @@ class ModManager:
 		latest_mod = self.get_latest_mod()
 		if latest_mod is not None:
 			for name in latest_mod.namelist():
-				if os.path.splitext(name)[1] == '_file_manifest.xml':
+				print(name)
+				if os.path.splitext(name)[1] == '.xml':
 					with latest_mod.open(name) as myfile:
 						remote_manifest = myfile.read()
-						remote_tree = ET.fromstring(remote_manifest)
-						remote_root = remote_tree.getroot()
+						print(remote_manifest)
+						remote_root = ET.fromstring(remote_manifest)
 						remote_published_utc = int(remote_root.get('published_utc'))
 						if remote_published_utc > published_utc:
+							print('I am here')
 							return False
-							#self.install_mod()
 						else:
 							return True
 		else:
 			print("No latest mod available")
 			return False
+		print("Unknown error. Try uninstalling and reinstalling mod.")
+		return False
 
 	def mod_cleanup(self):
 		# need to delete files within the local package directory if any exist
 		if os.path.isdir(self.localPkgDir()):
 			for file in os.listdir(self.localPkgDir()):
-				file_path = os.path.join(self.localPkgDir, file)
+				file_path = os.path.join(self.localPkgDir(), file)
 				try:
 					if os.path.isfile(file_path):
 						os.unlink(file_path)
 				except Exception as e:
 					print(e)
-		#os.rmdir(self.localPkgDir)
-		return
+		return "Mod removed."
 
 	def install_mod(self):
-		try:
-			response = requests.get(self.RELEASE_URI)
-			response.raise_for_status()
-		except requests.exceptions.RequestException as err:
-			print("Error Occured: ",err)
-			return False
-		else:
-			mod_package = zipfile.ZipFile(io.BytesIO(response.content))
-			os.makedirs(self.localPkgDir, exist_ok=True)
+		mod_package = self.get_latest_mod()
+		if mod_package is not None:
+			os.makedirs(self.localPkgDir(), exist_ok=True)
 			for name in mod_package.namelist():
 				if os.path.splitext(name)[1] == '.pkg':
-					mod_w_path = self.localPkgPath
+					mod_w_path = self.localPkgPath()
 				else:
-					mod_w_path = self.localManifestPath
+					mod_w_path = self.localManifestPath()
 
 				with mod_package.open(name) as myfile:
 					with open(mod_w_path, 'wb') as f:
 						f.write(myfile.read())
-			return True
+			print("Mod installation complete.")
+		else:
+			print("Unable to install mod.")
 
 	def status(self):
-		if os.path.isfile(self.localPkgPath) and os.path.isfile(self.localManifestPath) and self.check_mod_version():
-			return "Mod is installed and up to date."
-		elif os.path.isdir(self.localPkgDir):
-			pkg_files = [file for file in os.listdir(self.localPkgDir) if file.endswith('.pkg')]
-			manifest_files = [file for file in os.listdir(self.localPkgDir) if file.endswith('_file_manifest.xml')]
-			if len(pkg_files) == 1 and len(manifest_files) == 1:
-				return "Mod is installed but may be out of date."
-			else:
+		if os.path.isdir(self.localPkgDir()):
+			pkg_files = [file for file in os.listdir(self.localPkgDir()) if file.endswith('.pkg')]
+			manifest_files = [file for file in os.listdir(self.localPkgDir()) if file.endswith('_file_manifest.xml')]
+			if len(pkg_files) == 1 and len(manifest_files) == 1 and os.path.isfile(self.localPkgPath()):
+				if self.check_mod_version():
+					return "Mod is installed and up to date."
+				else:
+					return "Mod is outdated. Press I to install latest."
+			elif os.path.isfile(self.localPkgPath()):
 				return "Mod is installed but files are missing or corrupted."
+			else:
+				return "Mod is not installed."
 		else:
-			return "Mod is not installed."
+			return "HW2 local app data not found."
 
 
 if __name__ == '__main__':
@@ -139,9 +140,9 @@ if __name__ == '__main__':
 			mod_manager.mod_cleanup()
 			mod_manager.install_mod()
 		elif cmdKey == 'u' or cmdKey == 'U':
-			mod_manager.mod_cleanup()
+			print(mod_manager.mod_cleanup())
 		elif cmdKey == 's' or cmdKey == 'S':
-			mod_manager.status()
+			print(mod_manager.status())
 		elif cmdKey == 'q' or cmdKey == 'Q':
 			quit()
 		else:
